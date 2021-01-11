202258d1507b10b06e6bf6afacbc4713d3ea6069
CleverCoder42 <clevercoder42@gmail.com>
2021-01-11 17:42:59 -0500

    second comment

--- grocery-project-4/src/main/java/com/mayer/GroceryProject1Application.java
+++ grocery-project-4/src/main/java/com/mayer/GroceryProject1Application.java
@@ -11,6 +11,7 @@ public class GroceryProject1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(GroceryProject1Application.class, args);
 		System.out.println("=============4 -1!!!--VERSION GROCERY APP =======MASTER==============");
-		System.out.println("New comment >> 01-11-2021-1 <<);
+		System.out.println("New comment >> 01-11-2021-1 <<);"
+		System.out.println("New second comment >> 01-11-2021-1 <<);
 	}
 }
