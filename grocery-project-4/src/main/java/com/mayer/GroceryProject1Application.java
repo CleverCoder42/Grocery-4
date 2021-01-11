cda538aab3548b4c523ad9ef5bb86f72ac1dd0c4
vh-qa <holubenko.viacheslav@tech-stack.io>
2021-01-11 23:32:05 +0200

    New comment was added

--- grocery-project-4/src/main/java/com/mayer/GroceryProject1Application.java
+++ grocery-project-4/src/main/java/com/mayer/GroceryProject1Application.java
@@ -11,6 +11,6 @@ public class GroceryProject1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(GroceryProject1Application.class, args);
 		System.out.println("=============4 -1!!!--VERSION GROCERY APP =======MASTER==============");
+		System.out.println("New comment >> 01-11-2021-1 <<);
 	}
-
 }
